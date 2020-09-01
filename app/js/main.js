d52dbc1ea11298d7f037f2e3e2309e679e153e5e
garmanika <aleks.orlov97@gmail.com>
2020-09-01 16:49:56 +0300

    added product-slider and product

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,15 @@
 $(function(){
+    var mixer = mixitup('.products__inner-box');
+    $(".rate-star").rateYo({
+        rating: 5,
+        starWidth: "12px",
+        readOnly: true,
+      });
+      $('.product-slider__inner').slick({
+        arrows:false,
+        dots: true,
+        slidesToShow: 4,
+        slidesToScroll: 4,
 
-
+      });
 });
